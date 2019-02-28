0459a35a83ecfb87d036d275c78afc779d5f76d9
Karim Mohamed <komicho1996@gmail.com>
2019-02-28 22:30:18 +0200

    hash login route

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,5 +5,5 @@ Route::group([
     'namespace' => 'FoxLaby\Mudir\app\Http\Controllers',
     'middleware' => 'web'
 ], function () {
-    Route::get('/login', 'MudirController@login')->name('mudir.login');
+    // Route::get('/login', 'MudirController@login')->name('mudir.login');
 });
