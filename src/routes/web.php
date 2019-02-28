0acfe28d27b53cea5a6b618af736f49e5756cc3d
Karim Mohamed <komicho1996@gmail.com>
2019-02-28 20:41:56 +0200

    Create RouteServiceProvider

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
-Route::group(['prefix' => 'mudir', 'namespace' => 'FoxLaby\app\Http\Controllers', 'middleware' => 'web'], function () {
-    Route::get('/', 'MudirController@login')->name('mudir.login');
+Route::group([
+    'prefix' => 'mudir',
+    'namespace' => 'FoxLaby\app\Http\Controllers',
+    'middleware' => 'web'
+], function () {
+    Route::get('/login', 'MudirController@login')->name('mudir.login');
 });
